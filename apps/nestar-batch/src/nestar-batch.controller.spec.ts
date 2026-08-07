800f6db1df521cccdd1d321e825988f91befab9e
ryan <mr.yaryyev@gmail.com>
2026-08-08 07:43:47 +0900

    fix: installation packages & modify format standards

--- apps/nestar-batch/src/nestar-batch.controller.spec.ts
+++ apps/nestar-batch/src/nestar-batch.controller.spec.ts
@@ -3,20 +3,20 @@ import { NestarBatchController } from './nestar-batch.controller';
 import { NestarBatchService } from './nestar-batch.service';
 
 describe('NestarBatchController', () => {
-  let nestarBatchController: NestarBatchController;
+	let nestarBatchController: NestarBatchController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [NestarBatchController],
-      providers: [NestarBatchService],
-    }).compile();
+	beforeEach(async () => {
+		const app: TestingModule = await Test.createTestingModule({
+			controllers: [NestarBatchController],
+			providers: [NestarBatchService],
+		}).compile();
 
-    nestarBatchController = app.get<NestarBatchController>(NestarBatchController);
-  });
+		nestarBatchController = app.get<NestarBatchController>(NestarBatchController);
+	});
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(nestarBatchController.getHello()).toBe('Hello World!');
-    });
-  });
+	describe('root', () => {
+		it('should return "Hello World!"', () => {
+			expect(nestarBatchController.getHello()).toBe('Hello World!');
+		});
+	});
 });
