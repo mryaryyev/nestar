697faff78322ed75ad49131de178684f00cc3800
ryan <mr.yaryyev@gmail.com>
2026-08-14 07:59:06 +0900

    feat: integrate validation pipes logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -7,18 +7,18 @@ export class MemberService {
 	constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
 
 	public async signup(): Promise<string> {
-		return 'signup executed';
+		return await Promise.resolve('signup executed');
 	}
 
 	public async login(): Promise<string> {
-		return 'login executed';
+		return await Promise.resolve('login executed');
 	}
 
 	public async updateMember(): Promise<string> {
-		return 'updateMember executed';
+		return await Promise.resolve('updateMember executed');
 	}
 
 	public async getMember(): Promise<string> {
-		return 'getMember executed';
+		return await Promise.resolve('getMember executed');
 	}
 }
--- apps/nestar-api/src/libs/enums/member.enum.ts
+++ apps/nestar-api/src/libs/enums/member.enum.ts
@@ -24,5 +24,5 @@ export enum MemberAuthType {
 	TELEGRAM = 'TELEGRAM',
 }
 registerEnumType(MemberAuthType, {
-	name: 'MemberAutType',
+	name: 'MemberAuthType',
 });
