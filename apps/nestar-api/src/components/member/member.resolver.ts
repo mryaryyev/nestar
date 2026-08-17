c26b7f139049c2ed5ff66f91397b587f4f145dbf
ryan <mr.yaryyev@gmail.com>
2026-08-17 21:49:59 +0900

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,6 +1,5 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
 
@@ -10,36 +9,25 @@ export class MemberResolver {
 
 	@Mutation(() => Member)
 	public async signup(@Args('input') input: MemberInput): Promise<Member> {
-		try {
-			console.log('Mutation: signup');
-			console.log('input: ', input);
-			return this.memberService.signup(input);
-		} catch (err) {
-			console.log('Error, signup', err);
-			throw new InternalServerErrorException(err);
-		}
+		console.log('Mutation: signup');
+		return await this.memberService.signup(input);
 	}
 
 	@Mutation(() => Member)
 	public async login(@Args('input') input: LoginInput): Promise<Member> {
-		try {
-			console.log('Mutation: login');
-			return this.memberService.login(input);
-		} catch (err) {
-			console.log('Error, signup', err);
-			throw new InternalServerErrorException(err);
-		}
+		console.log('Mutation: login');
+		return await this.memberService.login(input);
 	}
 
 	@Mutation(() => String)
 	public async updateMember(): Promise<string> {
 		console.log('Mutation: updateMember');
-		return this.memberService.updateMember();
+		return await this.memberService.updateMember();
 	}
 
 	@Query(() => String)
 	public async getMember(): Promise<string> {
 		console.log('Query: getMember');
-		return this.memberService.getMember();
+		return await this.memberService.getMember();
 	}
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -18,8 +18,8 @@ export class MemberService {
 			// TODO: Authentication via TOKEN
 			return result;
 		} catch (err) {
-			console.log('Error, Service.model:', err);
-			throw new BadRequestException(err);
+			console.log('Error, Service.model:', err instanceof Error ? err.message : err);
+			throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
 		}
 	}
 
