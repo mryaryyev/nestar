269d7a9f66e23678c65007fe76b82163d926128b
ryan <mr.yaryyev@gmail.com>
2026-08-19 09:36:15 +0900

    feat: standard integration of logging interceptor

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -34,7 +34,7 @@ export class MemberService {
 		}
 
 		// TODO: Compare password
-		console.log('response:', response);
+		//console.log('response:', response);
 
 		const isMatch = memberPassword === response.memberPassword;
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
