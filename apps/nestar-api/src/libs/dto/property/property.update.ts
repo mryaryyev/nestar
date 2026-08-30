270cfece33958b9b3e2fe7122a7eebc8ec3d9b07
ryan <mr.yaryyev@gmail.com>
2026-08-31 01:03:11 +0900

    feat: develop getAgentProperties business logic

--- apps/nestar-api/src/libs/dto/property/property.update.ts
+++ apps/nestar-api/src/libs/dto/property/property.update.ts
@@ -18,7 +18,7 @@ export class PropertyUpdate {
 	propertyType?: PropertyType;
 
 	@IsOptional()
-	@Field(() => PropertyType, { nullable: true })
+	@Field(() => PropertyStatus, { nullable: true })
 	propertyStatus?: PropertyStatus;
 
 	@IsOptional()
